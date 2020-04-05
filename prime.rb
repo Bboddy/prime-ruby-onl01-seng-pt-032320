f6968fba097f67282a47c3d32c2b41ef66206573
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 08:02:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,3 @@
 def prime?(num)
-  result = true
-  if num
-    for i in 2..num -1
-      if num % i == 0 
-        result = false
-        break
-      end
-  else
-    return result
-  end
+  
 end
